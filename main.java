288295b0102dc8edcc22e14b48841c597ae055e3
akari2596 <148976204+akari2596@users.noreply.github.com>
2023-10-25 18:09:07 +0200

    new files added (#1)
    * new files added
    
    * pr changes done
    
    ---------
    
    Co-authored-by: Peter Szatmary <peter.szatmary@windowslive.com>

--- main.java
+++ main.java
@@ -1,8 +1,11 @@
-public class Program {
+public class Program2 {
 
    public static void main(String[] arguments) {
-      System.out.println("Hello world");
+      System.out.println("Hello world world");
    }
 
+   public String printer(){
+               return "hi";
+   }
 
 }
